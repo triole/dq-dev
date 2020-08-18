f9a4cf195cee53a5a1d653180b4d8b11c1d2c7b7
olaf michaelis <o.mic@web.de>
2020-08-18 12:46:08 +0200

    Add folder check

--- py/render_yaml.py
+++ py/render_yaml.py
@@ -3,12 +3,17 @@ import os
 import re
 import sys
 from os.path import join as pj
+from subprocess import PIPE, Popen
 
 import yaml
 
 scriptname = os.path.realpath(__file__)
 scriptdir = '/'.join(scriptname.split('/')[:-1])
 basedir = re.search('.*(?=/)', scriptdir).group(0)
+
+cend = '\033[0m'
+cwarn = '\033[91m'
+cyel = '\033[93m'
 
 parser = argparse.ArgumentParser(
     description=os.path.basename(__file__).title() + ': ' +
@@ -17,6 +22,20 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('config_file', nargs='?', default='conf.yaml', help='config file to read')
 args = parser.parse_args()
+
+
+def is_git(folder):
+    proc = Popen(
+        ['git', '-C', folder, 'remote', '-v'],
+        stdout=PIPE, stderr=PIPE, close_fds=True
+    )
+    (out, err) = proc.communicate()
+    exitcode = proc.wait()
+    if exitcode != 0:
+        return (False, None)
+    out = out.splitlines()[0].decode('utf-8')
+    out = re.search(r'git.*?\s', out).group(0)
+    return (True, out)
 
 
 def read_yaml(filename):
@@ -32,7 +51,7 @@ def save_yaml(data, filename):
         yaml.dump(data, outfile, default_flow_style=False, indent=4)
 
 
-def make_volume(name, device):
+def make_volume(name, device, required_git=False):
     vol = {}
     device = replace_vars(device)
     if os.path.isdir(device) is False:
@@ -41,11 +60,28 @@ def make_volume(name, device):
             '\". Path on host does not exist \"' + device + '\"'
         )
     else:
-        vol['driver_opts'] = {}
-        vol['driver_opts']['device'] = replace_vars(device)
-        vol['driver_opts']['o'] = 'bind'
-        vol['driver_opts']['type'] = 'none'
-        vol['name'] = name
+        if required_git is True:
+            ig = is_git(device)
+            if ig[0] is False:
+                print(
+                    cwarn +
+                    '\nFolder ' + cyel + device + cwarn +
+                    ' does not look like a git repo.\n' +
+                    'Please make sure that it really contains the source of ' +
+                    cyel + name + cend + '\n'
+                )
+                sys.exit(1)
+            else:
+                print(
+                    'App folder ' + cyel + device + cend + ' ' +
+                    'has git url ' + cyel + ig[1] + cend
+                )
+        else:
+            vol['driver_opts'] = {}
+            vol['driver_opts']['device'] = replace_vars(device)
+            vol['driver_opts']['o'] = 'bind'
+            vol['driver_opts']['type'] = 'none'
+            vol['name'] = name
     return vol
 
 
@@ -76,7 +112,7 @@ if __name__ == '__main__':
     if os.path.isfile(conf_file) is False:
         print('Can not find "' + conf_file + '"')
         sys.exit(1)
-    print('Read config file ' + conf_file)
+    print('Read config file ' + cyel + conf_file + cend)
     conf = read_yaml(conf_file)
     yd = read_yaml(pj(scriptdir, 'dc_template.yaml'))
 
@@ -87,7 +123,9 @@ if __name__ == '__main__':
         yd['volumes'][volname] = make_volume(volname, conf['folders'][volname])
 
     yd['volumes']['dq_app'] = make_volume(
-        'dq_app', conf['folders'][conf['active_app']['name']]
+        'dq_app',
+        conf['folders'][conf['active_app']['name']],
+        required_git=True,
     )
 
     # add volumes to services
