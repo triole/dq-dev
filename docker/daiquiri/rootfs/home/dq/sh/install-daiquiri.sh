d77b377c80bad07f5425576778b497cf440a0f2a
Olaf Michaelis <omichaelis@aip.de>
2020-08-19 11:20:05 +0200

    Improve healthcheck

--- docker/daiquiri/rootfs/home/dq/sh/install-daiquiri.sh
+++ docker/daiquiri/rootfs/home/dq/sh/install-daiquiri.sh
@@ -18,7 +18,7 @@ if [[ $(pip3 freeze | grep -Poc "django-daiquiri") == "0" ]]; then
 
     mkdir -p "${DQAPP}/vendor"
     python3 manage.py download_vendor_files
-    python3 manage.py collectstatic
+    python3 manage.py collectstatic >/dev/null 2>&1
     # python3 manage.py runserver 0.0.0.0:8000 &
 
 else
