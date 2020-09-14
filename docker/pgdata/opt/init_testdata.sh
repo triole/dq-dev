e3a72648be4a56ac82da7bea45a089f913955d43
Olaf Michaelis <omichaelis@aip.de>
2020-09-14 09:24:39 +0200

    Fix db folder permissions

--- docker/pgdata/opt/init_testdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fol="${HOME}/source/testing/sql/postgres/"
-role="daiquiri_data"
-
-psql -U ${role} daiquiri_data <"${fol}/data.sql"
-psql -U ${role} daiquiri_data <"${fol}/test.sql"
-psql -U ${role} test_daiquiri_data <"${fol}/data.sql"
-psql -U ${role} test_daiquiri_data <"${fol}/test.sql"
